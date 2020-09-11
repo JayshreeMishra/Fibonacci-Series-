4d3ace1a034937939c63862d92cb8bbe31c00c78
Jayshree Mishra <70279830+JayshreeMishra@users.noreply.github.com>
2020-09-11 20:43:55 +0530

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,15 @@
+# This program prints the fibonacci series of the given term
+n= int(input("Enter the term\n"))
+a=0
+b=1
+print("The fibonacci series upto the given term is\n",a)  
+print("",b) 
+for i in range(n):
+    c=a+b
+    print("",c)
+    a=b
+    b=c
+
+        
+         
+
